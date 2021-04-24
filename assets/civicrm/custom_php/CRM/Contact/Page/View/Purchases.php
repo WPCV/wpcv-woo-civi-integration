92a26c314cfe61a05e402d8e673ada9ecf27bb4d
Christian Wach <needle@haystack.co.uk>
2021-04-24 12:58:10 +0100

    Add Editor Config files and convert

--- assets/civicrm/custom_php/CRM/Contact/Page/View/Purchases.php
+++ assets/civicrm/custom_php/CRM/Contact/Page/View/Purchases.php
@@ -16,55 +16,55 @@ defined( 'ABSPATH' ) || exit;
  */
 class CRM_Contact_Page_View_Purchases extends CRM_Core_Page {
 
-	/**
-	 * Render the QuickForm page.
-	 *
-	 * @since 2.0
-	 */
-	function run() {
+  /**
+   * Render the QuickForm page.
+   *
+   * @since 2.0
+   */
+  public function run() {
 
-		CRM_Utils_System::setTitle( ts( 'Purchases' ) );
+    CRM_Utils_System::setTitle(ts('Purchases'));
 
-		$cid = CRM_Utils_Request::retrieve( 'cid', 'Positive', $this );
+    $cid = CRM_Utils_Request::retrieve('cid', 'Positive', $this);
 
-		$orders = WPCV_WPCV_WCI()->orders_tab->get_orders( $cid );
+    $orders = WPCV_WPCV_WCI()->orders_tab->get_orders($cid);
 
-		$this->assign( 'i18n', array(
-			'orderNumber' 	=> __('Order Number', 'wpcv-woo-civi-integration'),
-			'date' 			=> __('Date', 'wpcv-woo-civi-integration'),
-			'billingName' 	=> __('Billing Name', 'wpcv-woo-civi-integration'),
-			'shippingName' 	=> __('Shipping Name', 'wpcv-woo-civi-integration'),
-			'itemCount' 	=> __('Item count', 'wpcv-woo-civi-integration'),
-			'amount'		=> __('Amount', 'wpcv-woo-civi-integration'),
-			'actions' 		=> __('Actions', 'wpcv-woo-civi-integration'),
-			'emptyUid' 		=> __('This contact is not linked to any WordPress user or WooCommerce Customer', 'wpcv-woo-civi-integration'),
-			'orders' 		=> __('Orders', 'wpcv-woo-civi-integration'),
-			'addOrder' 		=> __('Add Order', 'wpcv-woo-civi-integration'),
-		) );
+    $this->assign('i18n', [
+      'orderNumber'  => __('Order Number', 'wpcv-woo-civi-integration'),
+      'date'         => __('Date', 'wpcv-woo-civi-integration'),
+      'billingName'  => __('Billing Name', 'wpcv-woo-civi-integration'),
+      'shippingName' => __('Shipping Name', 'wpcv-woo-civi-integration'),
+      'itemCount'    => __('Item count', 'wpcv-woo-civi-integration'),
+      'amount'       => __('Amount', 'wpcv-woo-civi-integration'),
+      'actions'      => __('Actions', 'wpcv-woo-civi-integration'),
+      'emptyUid'     => __('This contact is not linked to any WordPress user or WooCommerce Customer', 'wpcv-woo-civi-integration'),
+      'orders'       => __('Orders', 'wpcv-woo-civi-integration'),
+      'addOrder'     => __('Add Order', 'wpcv-woo-civi-integration'),
+    ]);
 
-		$this->assign( 'orders', $orders );
+    $this->assign('orders', $orders);
 
-		$uid = abs(CRM_Core_BAO_UFMatch::getUFId( $cid ));
-		if ( $uid ) {
+    $uid = abs(CRM_Core_BAO_UFMatch::getUFId($cid));
+    if ($uid) {
 
-			$url = add_query_arg(['post_type' => 'shop_order', 'user_id' => $uid], admin_url('post-new.php'));
+      $url = add_query_arg(['post_type' => 'shop_order', 'user_id' => $uid], admin_url('post-new.php'));
 
-			/**
-			 * Filter the URL for the new Order.
-			 *
-			 * @since 2.0
-			 *
-			 * @param str $url The URL for the new Order.
-			 * @param int $uid The numeric ID of the WordPress User.
-			 */
-			$new_order_url = apply_filters('wpcv_woo_civi/add_order/url', $url, $uid);
+      /**
+       * Filter the URL for the new Order.
+       *
+       * @since 2.0
+       *
+       * @param str $url The URL for the new Order.
+       * @param int $uid The numeric ID of the WordPress User.
+       */
+      $new_order_url = apply_filters('wpcv_woo_civi/add_order/url', $url, $uid);
 
-			$this->assign( 'newOrderUrl', $new_order_url );
+      $this->assign('newOrderUrl', $new_order_url);
 
-		}
+    }
 
-		parent::run();
+    parent::run();
 
-	}
+  }
 
 }
